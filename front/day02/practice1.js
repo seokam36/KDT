795e1eb06302f8af660d7e4ca0ef56701a15ff85
seokam36 <seokam36@gmail.com>
2026-07-07 17:23:41 +0900

    HTML 마크업 practice 문제풀이

---
+++ front/day02/practice1.js
@@ -0,0 +1,8 @@
+let name = prompt("이름")
+console.log(name)
+
+let beverage = prompt("음료")
+console.log(beverage)
+
+let order = prompt("주문 수량")
+console.log(order)
---
+++ front/day02/practice2.js
@@ -0,0 +1,16 @@
+let name = prompt("이름")
+let exercise = prompt("운동 종류")
+let minutes = prompt("운동 시간(분 단위)")
+
+console.log("[운동 기록]")
+console.log("이름")
+console.log(name)
+
+console.log("운동 종류")
+console.log(exercise)
+
+console.log("운동 시간")
+console.log(minutes)
+
+console.log("칼로리")
+console.log(minutes*7)
