062fd7130a951abf4ed770c0f231bd3258ceffdf
seokam36 <seokam36@gmail.com>
2026-07-20 15:44:10 +0900

    JS_practice5

--- front/day10/practice5.js
+++ front/day10/practice5.js
@@ -16,7 +16,7 @@ const members = [
 { id: 'user2', password: 'pass2', name: '사용자2' },
 ];
 */
-const members = [
+/* const members = [
 { id: 'user1', password: 'pass1', name: '사용자1' },
 { id: 'user2', password: 'pass2', name: '사용자2' },
 ]
@@ -36,7 +36,8 @@ if(check==true){
     console.log("이미 존재하는 아이디입니다")
 } else{
     members.push(member)
-}
+    console.log(members)
+} */
 
 /*문제 3: 객체 배열의 속성 값 평균 구하기
 scores 배열에 담긴 모든 학생의 수학(math) 점수 평균을 계산하여 콘솔에 출력하시오.
@@ -46,6 +47,16 @@ const scores = [
 { name: 'C', math: 76, science: 78 }
 ];
 */
+const scores = [
+{ name: 'A', math: 80, science: 92 },
+{ name: 'B', math: 95, science: 88 },
+{ name: 'C', math: 76, science: 78 }
+];
+let sum = 0
+for(let i=0; i<scores.length; i++){
+    sum += scores[i].math
+}
+console.log(sum/scores.length)
 
 
 /*문제 4: 특정 조건을 만족하는 객체 찾기
@@ -57,6 +68,24 @@ const products = [
 { id: 4, name: '딸기' }
 ];
 */
+let check = false
+const products = [
+{ id: 1, name: '사과' },
+{ id: 2, name: '바나나' },
+{ id: 3, name: '포도' },
+{ id: 4, name: '딸기' }
+];
+for(let i=0; i<products.length; i++){
+    if(products[i].id == 3){
+        check = true
+        console.log(products[i])
+        break
+    }
+}
+if(check == false){
+    console.log("상품을 찾을 수 없습니다")
+}
+
 
 /*문제 5: 객체 배열 필터링하기
 users 배열에서 isActive가 true인 사용자들만으로 구성된 새로운 배열 activeUsers를 만들고, 이 배열을 콘솔에 출력하시오.
@@ -67,31 +96,54 @@ const users = [
 { id: 4, name: '유저4', isActive: false }
 ];
 */
+const users = [
+{ id: 1, name: '유저1', isActive: true },
+{ id: 2, name: '유저2', isActive: false },
+{ id: 3, name: '유저3', isActive: true },
+{ id: 4, name: '유저4', isActive: false }
+];
+let arr = []
+for(let i=0; i<users.length; i++){
+    if(users[i].isActive == true){
+        arr.push(users[i])
+    }
+}
+console.log(arr)
+
 
 /*문제 6: 객체 배열 데이터 변환하기
 movies 배열에 있는 각 영화 객체에서 title 속성만 추출하여, 영화 제목들로만 이루어진 새로운 배열 movieTitles를 만들고 콘솔에 출력하시오.
+*/
 const movies = [
 { title: '인셉션', director: '크리스토퍼 놀란' },
 { title: '기생충', director: '봉준호' },
 { title: '매트릭스', director: '워쇼스키 자매' }
 ];
-*/
+let title = []
+for(let i=0; i<movies.length; i++){
+    title.push(movies[i].title)
+}
+console.log(title)
+
 
 /*문제 7: 데이터 그룹화하기
 다음 team 배열을 department를 기준으로 그룹화하여, 아래 result와 같은 형태로 만드시오.
-const team = [
-{ name: '철수', department: '개발팀' },
-{ name: '영희', department: '기획팀' },
-{ name: '민수', department: '개발팀' },
-{ name: '지혜', department: '기획팀' }
-];
 최종 결과 형태 (result)
 {
 '개발팀': ['철수', '민수'],
 '기획팀': ['영희', '지혜']
 }
 */
-
+const team = [
+{ name: '철수', department: '개발팀' },
+{ name: '영희', department: '기획팀' },
+{ name: '민수', department: '개발팀' },
+{ name: '지혜', department: '기획팀' }
+];
+let result2 = {"개발팀":[], "기획팀":[]}
+for(let i=0; i<team.length; i++){
+    
+}
 /* 문제 8: 장바구니 총액 계산하기
 고객의 장바구니 정보를 담은 cart 배열과 상품 정보를 담은 productsInfo 배열이 있습니다.
 cart 배열: 각 요소는 고객이 담은 상품의 id와 quantity(수량)를 가집니다.
