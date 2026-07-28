821c4c53375f88913066a2af43ac04a120d94ca0
seokam36 <seokam36@gmail.com>
2026-07-28 11:15:00 +0900

    java_exam1

---
+++ back/Java_Study/src/day01/Exam1.java
@@ -0,0 +1,11 @@
+
+package day01;
+
+public class Exam1 {
+    int a = 10;
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        int b = 10;
+        
+    }
+}
